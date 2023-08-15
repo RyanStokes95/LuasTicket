4d3492a9cd329eb28e36d376b26dc1b2e10a57d0
RyanStokes95 <ryanstokes560@gmail.com>
2023-08-15 21:24:28 +0100

    Instantiable and app class complete v1

--- src/LuasTicket.java
+++ src/LuasTicket.java
@@ -1,2 +1,49 @@
 public class LuasTicket {
+
+    private String person;
+    private int zone;
+    private double price;
+
+    public LuasTicket(){
+        person="";
+        zone=0;
+        price=0.0;
+    }
+
+    public void setPerson(String person) {
+        this.person = person;
+    }
+
+    public void setZone(int zone) {
+        this.zone = zone;
+    }
+
+    public void compute(){
+        if (person.equals("adult")){
+            if (zone==1){
+                price=1.50;
+            }
+            else if (zone==2) {
+                price=2.30;
+            }
+            else {
+                price=2.90;
+            }
+        }
+        else{
+            if (zone==1){
+                price=1.00;
+            }
+            else if (zone==2) {
+                price=1.00;
+            }
+            else {
+                price=1.20;
+            }
+        }
+    }
+
+    public double getPrice() {
+        return price;
+    }
 }
--- src/LuasTicketApp.java
+++ src/LuasTicketApp.java
@@ -1,2 +1,27 @@
+import java.util.*;
 public class LuasTicketApp {
+
+    public static void main(String[] args) {
+
+        String person="";
+        int zone=0;
+        double price=0.0;
+
+        Scanner keyboard=new Scanner(System.in);
+        LuasTicket myLuasticket=new LuasTicket();
+
+        System.out.println("Please enter if you are an adult or a child");
+        person=keyboard.nextLine();
+        myLuasticket.setPerson(person);
+
+        System.out.println("Please enter how many zones you wish to travel");
+        zone=keyboard.nextInt();
+        myLuasticket.setZone(zone);
+
+        myLuasticket.compute();
+
+        price= myLuasticket.getPrice();
+        System.out.println("Your fare is "+price);
+    }
+
 }
